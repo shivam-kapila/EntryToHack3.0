deb26b09fc7361000a2dbbe9ba124de3cdd6fb82
utkarshsingh99 <utkarshsingh369@gmail.com>
2019-01-25 14:58:46 +0530

    Team Registration Done

--- public/js/teamRegistration.js
+++ public/js/teamRegistration.js
@@ -7,27 +7,87 @@ submitForm.css('display', 'none');
 
 jQuery('#saveDetails').on('click', function () {
     var memberInput = takeInputValues(memberNumber++);
-    jQuery('#memberNumber')[0].innerHTML = `Enter Member ${memberNumber + 1}'s details:`;
-    clearInputValues();
-    console.log(teamDetails);
-    teamDetails.push(memberInput);
-    if(memberNumber === 3) {
-        submitForm.css('display', 'block');
-        jQuery('#saveDetails').css('display', 'none');
-    } 
+    if(validateMemberYear(memberInput))
+    {
+        jQuery('#memberNumber')[0].innerHTML = `Enter Member ${memberNumber + 1}'s details:`;
+        clearInputValues();
+        console.log(teamDetails);
+        teamDetails.push(memberInput);
+        if (memberNumber === 4) {
+            submitForm.css('display', 'block');
+            jQuery('#saveDetails').css('display', 'none');
+        }
+        if (memberNumber === 3) {
+            submitForm.css('display', 'block');
+        }
+    } else {
+        memberInput--;
+        alert("Roll Number and Year do not match! Enter member details again.")
+    }
+        
 });
 
 submitForm.on('click', function () {
-    validateForm();             // TODO: Function to be made
-    jQuery.post('/team/student', teamDetails, function (data, e) {
-        if(e) {
-            alert('Some error has occurred');
-        } else {
-            alert('Congratulations, you have registered!');
-        }
+    if(validateForm()) {
 
-    })
+        jQuery.post('/team/student', teamDetails, function (data, e) {
+            if(e) {
+                alert('Some error has occurred');
+            } else {
+                alert('Congratulations, you have registered!');
+            }
+        });
+    } else {
+        alert("This team formation is not allowed. Please refer to the rules again! http://csec.nith.ac.in/hack/");
+    }
 });
+
+function validateForm() {                                         
+    var second = 0, third = 0, first = 0, fourth = 0, fifth = 0;
+    teamDetails.forEach(function (member) {
+        switch(member.year) {
+            case 2: second++;
+                break;
+            case 3: third++;
+                break;
+            case 4: fourth++;
+                break;
+            case 5: fifth++;
+                break;
+            case 1: first++;
+                break;
+            default: break;     // No use        
+        }
+        if(second > 0) {
+            if(third > 0 || fourth > 0 || fifth > 0 || second > 3) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+        if(third > 0) {
+            if (second > 0 || fourth > 0 || fifth > 0 || third > 2) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+        if(fourth > 0) {
+            if (third > 0 || second > 0 || fifth > 0 || fourth > 1) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+        if (fifth > 0) {
+            if (third > 0 || second > 0 || fourth > 0 || fifth > 1) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    });
+}
 
 function takeInputValues(memberNumber) {
     return {
@@ -54,4 +114,15 @@ function clearInputValues() {
     jQuery('input[name=skills1]').val('');
     jQuery('input[name=skills2]').val('');
     jQuery('input[name=skills3]').val('');
+}
+
+function validateMemberYear(member) {
+    var currentYear = 19;
+    var memberYear = currentYear - member.year;            // Will return 18, 17, 16, etc
+    var memberRollNumber = member.rollNumber;
+    if(memberRollNumber.search(memberYear) === -1) {        // MemberYear wasn't found in MemberRollNumber
+        return false;
+    } else {
+        return true;
+    }
 }
