c484fd07c14754e8f25559c760776c3879499736
Paras Gupta <parasg1999@gmail.com>
2019-02-02 01:11:52 +0530

    Admin Dashboard Complete

--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,46 @@ var express = require("express");
 var router = express.Router();
 var Admin = require("../models/admin");
 var Mentor = require("../models/mentor");
+var Team = require("../models/team");
 var async = require("async");
 var nodemailer = require("nodemailer");
 var passport = require("passport");
 
 router.get("/login", function (req, res) {
   res.render("adminLogin");
+});
+
+router.get("/teams", isAdminLoggedIn, function (req, res) {
+  Team.find({} , function (err, teams) {
+    if (err) {
+      console.log(err);
+      res.redirect("back");
+    } else {
+      res.render("adminTeams", { teams });
+    }
+  });
+});
+
+router.get("/mentorChallenges", isAdminLoggedIn, function (req, res) {
+  Mentor.find({} , function (err, mentors) {
+    if (err) {
+      console.log(err);
+      res.redirect("back");
+    } else {
+      res.render("adminMentorChallenges", { mentors });
+    }
+  });
+});
+
+router.get("/team/:id", isAdminLoggedIn, function (req, res) {
+  Team.findOne({_id: req.params.id} , function (err, team) {
+    if (err) {
+      console.log(err);
+      res.redirect("back");
+    } else {
+      res.render("adminTeamDashboard", { team });
+    }
+  });
 });
 
 router.post("/login", passport.authenticate("admin",
@@ -38,12 +72,16 @@ router.post("/signup", function (req, res) {
 });
 
 router.get("/dashboard", isAdminLoggedIn, function (req, res) {
+    res.render("adminDashboard");
+});
+
+router.get("/mentors", isAdminLoggedIn, function (req, res) {
   Mentor.find({}, function (err, mentors) {
     if (err) {
       console.log(err);
       res.redirect("back");
     } else {
-      res.render("adminDashboard", { mentors: mentors });
+      res.render("adminMentors", { mentors: mentors });
     }
   });
 });
@@ -52,7 +90,7 @@ router.post("/:id/verify", isAdminLoggedIn, function (req, res) {
   Mentor.findById(req.params.id, function (err, mentor) {
     mentor.isVerified = "Verified";
     mentor.save();
-    res.redirect("/admin/dashboard");
+    res.redirect("/admin/mentors");
   });
 });
 
@@ -60,7 +98,7 @@ router.post("/:id/reject", isAdminLoggedIn, function (req, res) {
   Mentor.findById(req.params.id, function (err, mentor) {
     mentor.isVerified = "Rejected";
     mentor.save();
-    res.redirect("/admin/dashboard");
+    res.redirect("/admin/mentors");
   });
 });
 
