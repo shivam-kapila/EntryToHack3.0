0d8c6231f5f381a8d766528c440327f8e22e4234
shivam-kapila <shivamkapila4@gmail.com>
2019-02-03 02:01:47 +0530

    Database URL update

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ var indexRoutes = require("./routes/index");
 var mentorRoutes = require("./routes/mentor");
 var teamRoutes = require("./routes/team");
 var adminRoutes = require("./routes/admin");
-var url = process.env.DATABASEURL || "mongodb://localhost/entry_to_hack3";
+var url = 'mongodb://CSECHack3:csechack3@ds119795.mlab.com:19795/entry_to_hack3';
+// || "mongodb://localhost/entry_to_hack3";
 mongoose.connect(url, {
     useNewUrlParser: true
 });
